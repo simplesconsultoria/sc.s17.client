e72f4ebdc1d03638515b7e34cd405e9505dd0899
Gustavo Lepri <gustavolepri@gmail.com>
2011-09-01 10:19:04 -0300

    initial import

---
+++ sc/s17/client/config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# File: config.py
+
+__author__ = """'Simples Consultoria'"""
+__docformat__ = 'plaintext'
+PROJECTNAME = 'sc.s17.client'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+}
+
