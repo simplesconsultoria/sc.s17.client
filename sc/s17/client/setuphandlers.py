8f1ab5cedf623fe1a83b0afda4d03eb58ae4c727
hvelarde <hector.velarde@gmail.com>
2011-09-02 11:02:22 -0300

    removed deprecated code

--- sc/s17/client/setuphandlers.py
+++ sc/s17/client/setuphandlers.py
@@ -1,18 +1,9 @@
 # -*- coding: utf-8 -*-
-from zope import component
+
 import logging
+
 from Products.CMFCore.utils import getToolByName
-from Products.GenericSetup import interfaces as gsinterfaces
 from Products.GenericSetup.upgrade import listUpgradeSteps
-
-from Products.ZCatalog.ProgressHandler import ZLogHandler
-
-try:
-    from Products.CacheSetup import interfaces
-    from Products.CacheSetup.enabler import enableCacheFu
-    CACHEFU = True
-except ImportError:
-    CACHEFU = False
 
 _PROJECT = 'sc.s17.client'
 _PROFILE_ID = 'sc.s17.client:default'
@@ -25,15 +16,10 @@ def doUpgrades(context):
         return
     logger = logging.getLogger(_PROJECT)
     site = context.getSite()
-    setup_tool = getToolByName(site,'portal_setup')
-    cache = CACHEFU and getToolByName(context,'portal_cache_settings',None)
+    setup_tool = getToolByName(site, 'portal_setup')
     version = setup_tool.getLastVersionForProfile(_PROFILE_ID)
-    upgradeSteps = listUpgradeSteps(setup_tool,_PROFILE_ID, version)
-    sorted(upgradeSteps,key=lambda step:step['sortkey'])
-
-    if cache:
-        # Disable cachefu to avoid a tsunami of purge requests
-        cache.setEnabled(False)
+    upgradeSteps = listUpgradeSteps(setup_tool, _PROFILE_ID, version)
+    sorted(upgradeSteps, key=lambda step: step['sortkey'])
 
     for step in upgradeSteps:
         oStep = step.get('step')
@@ -43,8 +29,3 @@ def doUpgrades(context):
                                                           _PROFILE_ID)
             setup_tool.setLastVersionForProfile(_PROFILE_ID, oStep.dest)
             logger.info(msg)
-
-    if cache:
-        # Its ok to enable it again
-        cache.setEnabled(True)
-
