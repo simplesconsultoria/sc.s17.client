8db60774895e9b1955ff413a8154a4b019e9886d
hvelarde <hector.velarde@gmail.com>
2011-10-10 10:05:25 -0300

    removed optionflags

--- sc/s17/client/tests/test_doctest.py
+++ sc/s17/client/tests/test_doctest.py
@@ -7,15 +7,12 @@ from plone.testing import layered
 
 from sc.s17.client.testing import FUNCTIONAL_TESTING
 
-optionflags = doctest.REPORT_ONLY_FIRST_FAILURE
-
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
         layered(doctest.DocFileSuite('tests/functional.txt',
-                                     package='sc.s17.client',
-                                     optionflags=optionflags),
+                                     package='sc.s17.client'),
                 layer=FUNCTIONAL_TESTING),
         ])
     return suite
